578654111e4d00c4592f9f028f993349e9163611
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-08 21:34:05 -0500

    User may sign out

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -1,34 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'user signs out', %Q{
+feature "user signs out", %{
   As an authenticated user
   I want to sign out
-  So that my identity is forgotten about on the machine I'm using
+  So that my identity is forgotten on the machine I"m using
 } do
 
-  # Acceptance Criteria
-  # * If I'm signed in, I have an option to sign out
-  # * When I opt to sign out, I get a confirmation that my identity has been
-  #   forgotten on the machine I'm using
+  let(:user) { FactoryGirl.create(:user) }
 
-  scenario 'authenticated user signs out' do
-    user = FactoryGirl.create(:user)
+  scenario "authenticated user signs out" do
+    sign_in(user)
 
-    visit new_user_session_path
+    visit root_path
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-
-    click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully')
-
-    click_link 'Sign Out'
-    expect(page).to have_content('Signed out successfully')
+    click_link "Sign Out"
+    expect(page).to have_content("Signed out successfully")
   end
 
-  scenario 'unauthenticated user attempts to sign out' do
-    visit '/'
-    expect(page).to_not have_content('Sign Out')
+  scenario "unauthenticated user attempts to sign out" do
+    visit "/"
+    expect(page).to_not have_content("Sign Out")
   end
 end
