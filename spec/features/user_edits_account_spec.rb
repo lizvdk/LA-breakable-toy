a0e8d2f486183a037ded43e2aa62b140e5131401
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-08 22:38:17 -0500

    User may edit their account

---
+++ spec/features/user_edits_account_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature "user edits account", %{
+  As a user
+  I want to edit account
+  So that I can update my information
+} do
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario "user may edit their own profile" do
+    sign_in user
+
+    visit root_path
+
+    click_link user.username
+
+    click_link "Edit Account"
+
+    fill_in "Username", with: "changedusername1"
+    fill_in "Current password", with: user.password
+    click_button "Update"
+
+    expect(page).to have_content "Your account has been updated successfully"
+
+    expect(page).to have_content "changedusername1"
+  end
+end
