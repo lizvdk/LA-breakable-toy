2734c7a30959b9911ea8f7869a9d7d90a1bb3e01
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-16 14:28:24 -0500

    Mapbox basics working

--- app/assets/javascripts/reports/new/map.js
+++ app/assets/javascripts/reports/new/map.js
@@ -1,72 +1,31 @@
 function drawNewReportMap(){
-  var geocoder = new google.maps.Geocoder();
-  var placeSearch, autocomplete;
+  L.mapbox.accessToken = "pk.eyJ1IjoibGl6dmRrIiwiYSI6IlJodmpRdzQifQ.bUxjjqfXrx41XRFS7cXnIA";
 
-  function geocodePosition(pos) {
-    geocoder.geocode({
-      latLng: pos
-    }, function(responses) {
-      if (responses && responses.length > 0) {
-        updateMarkerAddress(responses[0].formatted_address);
-      } else {
-        updateMarkerAddress('Cannot determine address at this location.');
-      }
-    });
-  }
+  var map = L.mapbox.map('map', 'lizvdk.ko5f732m')
+  .addControl(L.mapbox.geocoderControl('mapbox.places-v1', {
+    keepOpen: true,
+    autocomplete: true
+  }));
 
-  function updateMarkerPosition(latLng) {
-    document.getElementById('report_latitude').value =
-    latLng.lat()
-    document.getElementById('report_longitude').value =
-    latLng.lng()
-  }
-
-  function updateMarkerAddress(str) {
-    document.getElementById('report_address').value = str;
-  }
-
-  function initialize() {
-    autocomplete = new google.maps.places.Autocomplete(
-      /** @type {HTMLInputElement} */(document.getElementById('report_address')),
-    { types: ['geocode'] });
-    var latLng = new google.maps.LatLng(42.3603, -71.058);
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      latLng = autocomplete.getPlace();
-    });
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: latLng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    var marker = new google.maps.Marker({
-      position: latLng,
-      title: 'Point A',
-      map: map,
+  function onLocationFound(e) {
+    var marker = L.marker(e.latlng, {
+      icon: L.mapbox.marker.icon({ 'marker-color': '#f86767' }),
       draggable: true
+    }).addTo(map);
+
+    marker.on('dragend', function (e) {
+      document.getElementById('report_latitude').value = marker.getLatLng().lat;
+      document.getElementById('report_longitude').value = marker.getLatLng().lng;
     });
 
-    // Update current position info.
-    updateMarkerPosition(latLng);
-    geocodePosition(latLng);
-
-    // Add dragging event listeners.
-    google.maps.event.addListener(marker, 'dragstart', function() {
-      updateMarkerAddress('Dragging...');
-    });
-
-    google.maps.event.addListener(marker, 'drag', function() {
-      updateMarkerPosition(marker.getPosition());
-    });
-
-    google.maps.event.addListener(marker, 'dragend', function() {
-      geocodePosition(marker.getPosition());
-    });
   }
 
-  function fillInAddress() {
-    var place = autocomplete.getPlace();
+  function onLocationError(e) {
+    alert(e.message);
   }
 
-  // Onload handler to fire off the app.
-  google.maps.event.addDomListener(window, 'load', initialize);
-};
+  map.on('locationfound', onLocationFound);
+  map.on('locationerror', onLocationError);
+
+  map.locate({setView: true, maxZoom: 16});
+}
