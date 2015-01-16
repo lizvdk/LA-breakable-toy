f0440c04e81073f3a2832fc41472068d77197c17
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-15 20:03:30 -0500

    Minor cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,12 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"]).first
+    user = User.where(email: ["email"]).first
 
     unless user
-      short_name = data["name"].slice!(0..8)
       user = User.create(username: "user#{User.all.last.id + 1}",
-        email: data["email"],
-        password: Devise.friendly_token[0,20]
+                         email: data["email"],
+                         password: Devise.friendly_token[0,20]
       )
     end
     user
