8c4d68e683c8f4194de67b945fd0bc5d211aca1b
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-18 11:22:31 -0500

    Hide EDIT button from other users

--- spec/features/user_edits_report_spec.rb
+++ spec/features/user_edits_report_spec.rb
@@ -6,17 +6,22 @@ feature "user edits report", %{
   So that I can correct or update the information
   } do
 
-    let(:report) { FactoryGirl.create(:report) }
+  let(:report) { FactoryGirl.create(:report) }
 
-    scenario "user may edit their own profile" do
-      sign_in report.user
-      visit report_path(report)
-      click_link "Edit Report"
+  scenario "user may edit their own profile" do
+    sign_in report.user
+    visit report_path(report)
+    click_link "Edit Report"
 
-      fill_in "Description", with: "New description"
-      click_button "Submit"
+    fill_in "Description", with: "New description"
+    click_button "Submit"
 
-      expect(page).to have_content "Your report has been updated successfully"
-      expect(page).to have_content "New description"
-    end
+    expect(page).to have_content "Your report has been updated successfully"
+    expect(page).to have_content "New description"
   end
+
+  scenario "user may not edit another user's report" do
+    visit report_path(report)
+    expect(page).not_to have_content "Edit Review"
+  end
+end
