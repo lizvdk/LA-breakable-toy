a089173276529ab7068d94fcf86cd0e2ba1ea433
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-18 13:45:30 -0500

    Admin may update a report's status

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :reports
 
+  namespace :admin do
+    resources :dashboards, only: [:index]
+    resources :reports, only: [:index, :edit, :update]
+  end
+
   match "/users/:id/finish_signup" => "users#finish_signup",
         via: [:get, :patch], as: :finish_signup
 end
