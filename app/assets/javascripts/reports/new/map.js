43048e5230c3330dd532c03e17d56787eb46340e
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-01-25 19:15:44 -0500

    Break js into smaller reuseable methods

--- app/assets/javascripts/reports/new/map.js
+++ app/assets/javascripts/reports/new/map.js
@@ -1,53 +1,44 @@
 function drawNewReportMap(){
   L.mapbox.accessToken = "pk.eyJ1IjoibGl6dmRrIiwiYSI6IlJodmpRdzQifQ.bUxjjqfXrx41XRFS7cXnIA";
 
-  var geocoder = L.mapbox.geocoder('mapbox.places');
   var map = L.mapbox.map('map', 'lizvdk.ko5f732m', { zoomControl: false })
-    .setView([42.3603, -71.0580], 20);
+  .setView([42.3603, -71.0580], 20);
+  var geocoder = L.mapbox.geocoder('mapbox.places');
   new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
   var marker = L.marker(new L.LatLng(42.3603, -71.0580), {
     icon: L.mapbox.marker.icon({'marker-color': 'C14B7A', 'marker-size' : 'large' }),
     draggable: true
   });
-  marker.addTo(map);
+  var $searchBox = $('.leaflet-control-mapbox-geocoder-form').children().first();
+  var $formAddress = $('#report_address');
+  var $formLat = $('#report_latitude');
+  var $formLng = $('#report_longitude');
 
-  function onLocationFound(e) {
-    marker.setLatLng(e.latlng).update();
-    marker.on('dragend', function (e) {
-      $('#report_latitude').val(marker.getLatLng().lat);
-      $('#report_longitude').val(marker.getLatLng().lng);
-    });
-
+  function ondragend() {
+    var m = marker.getLatLng();
+    $formLat.val(marker.getLatLng().lat);
+    $formLng.val(marker.getLatLng().lng);
   }
 
-  function onLocationError(e) {
-    alert(e.message);
-  }
-
-  map.on('locationfound', onLocationFound);
-  map.on('locationerror', onLocationError);
-  map.locate({setView: true, maxZoom: 16});
-
-  $(function() {
-
-    var $searchBox = $('.leaflet-control-mapbox-geocoder-form').children().first();
-    var $formAddress = $('#report_address');
-
-    $formAddress.focusout( function(e) {
+  function geocodeAddress () {
+    $formAddress.change(function(e) {
       var address = $formAddress.val();
       geocoder.query(address, populateLatLong);
     });
-  });
+  }
 
   function populateLatLong(err, data) {
     var lat = data.latlng[0];
     var lng = data.latlng[1];
-    $('#report_latitude').val(lat);
-    $('#report_longitude').val(lng);
+    $formLat.val(lat);
+    $formLng.val(lng);
 
     marker.setLatLng(data.latlng).update();
-
     map.panTo(data.latlng);
   }
 
+  marker.addTo(map);
+  marker.on('dragend', ondragend);
+  ondragend();
+  geocodeAddress();
 }
