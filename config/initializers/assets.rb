79741d7fc2aeb09ea26b3dcdc5ef47267a050abb
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-02-08 15:05:41 -0500

    Fix flaticon path to resolve error msg

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( reports/index/map.js )
 Rails.application.config.assets.precompile += %w( reports/new/map.js )
+Rails.application.config.assets.precompile += %w( vendor/flaticon.css )
